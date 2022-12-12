bba27613af0fb2023ff64603dde380821c32fe1c
Blessy <blessyvarghese93@gmail.com>
2022-12-12 14:25:59 +0400

    ch10

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname + '/dist/frontend')));
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null,__dirname+'/public/uploads');
+        cb(null,'/public/uploads');
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
