72bc990071293f1a2d2048ffed3f2977b22a654b
Blessy <blessyvarghese93@gmail.com>
2022-12-12 12:49:51 +0400

    ch

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use('/uploads', express.static(path.join(__dirname +'/uploads')));
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads')
+        cb(null, '/uploads')
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
